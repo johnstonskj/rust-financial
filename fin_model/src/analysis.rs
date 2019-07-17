a1bdd65affeae76352bc2916046b2afce8ffd583
Simon Johnston <johnstonskj@gmail.com>
2019-07-17 07:53:25 -0700

    fixing analysis types

--- fin_model/src/analysis.rs
+++ fin_model/src/analysis.rs
@@ -3,7 +3,6 @@ Provides structs and traits that represent common market analysis.
 */
 
 use std::collections::HashMap;
-use std::time::Duration;
 
 use crate::prelude::*;
 use crate::reporting::FinancialPeriod;
@@ -16,6 +15,7 @@ use crate::reporting::FinancialPeriod;
 pub type Counter = u32;
 
 /// The type of analyst recommendation/position.
+#[derive(PartialEq, Eq, Hash)]
 pub enum RatingType {
     Buy,
     Hold,
@@ -30,8 +30,6 @@ pub struct Ratings {
     pub ratings: HashMap<RatingType, Counter>,
     /// a standardized represention of the consensus of recommendations
     pub scale_mark: Option<f32>,
-    ///
-    pub averaged_over: Option<Duration>
 }
 
 /// Consensus price targets; high, low, and average.
@@ -47,7 +45,8 @@ pub struct EPSConsensus {
     pub consensus: Money,
     pub number_of_estimates: Counter,
     pub fiscal_period: FinancialPeriod,
-    pub fiscal_end_date: DateTime,
+    pub fiscal_end_date: Date,
+    pub next_report_date: Date
 }
 
 // ------------------------------------------------------------------------------------------------
@@ -60,7 +59,7 @@ pub struct EPSConsensus {
 pub trait Peers {
 
     /// Return a set of peer symbols.
-    fn peers(&self, for_symbol: Symbol) -> RequestResult<Snapshot<Symbols>>;
+    fn peers(&self, for_symbol: Symbol) -> RequestResult<Symbols>;
 }
 
 /// This trait is implemented by providers to return various analyst recommendations.
@@ -68,7 +67,7 @@ pub trait AnalystRecommendations {
 
     fn target_price(&self, for_symbol: Symbol) -> RequestResult<Snapshot<PriceTarget>>;
 
-    fn consensus_rating(&self, for_symbol: Symbol) -> RequestResult<Bounded<Ratings>>;
+    fn consensus_rating(&self, for_symbol: Symbol) -> RequestResult<Vec<Bounded<Ratings>>>;
 
-    fn consensus_eps(&self, for_symbol: Symbol) -> RequestResult<EPSConsensus>;
+    fn consensus_eps(&self, for_symbol: Symbol) -> RequestResult<Vec<EPSConsensus>>;
 }
