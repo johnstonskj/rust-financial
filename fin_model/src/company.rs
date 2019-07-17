cdabb44cfcabee503e4f17d9b82dfa8528eae5a0
Simon Johnston <johnstonskj@gmail.com>
2019-07-17 12:51:45 -0700

    updated company info structs

--- fin_model/src/company.rs
+++ fin_model/src/company.rs
@@ -55,30 +55,79 @@ pub struct IncomeStatement {
 pub struct BalanceSheet {
     pub current_cash: i64,
     pub short_term_investments: i64,
-    pub receivables: i64,
+    pub net_receivables: i64,
     pub inventory: i64,
-    pub current_assets: i64,
     pub other_current_assets: i64,
-    pub intangible_assets: i64,
-    pub other_assets: i64,
-    pub total_assets: i64,
+    pub total_current_assets: i64,
+
     pub long_term_investments: i64,
     pub property_plant_equipment: i64,
     pub goodwill: i64,
+    pub intangible_assets: i64,
+    pub accumulated_amortization: i64,
+    pub other_assets: i64,
+    pub deferred_asset_charges: i64,
+    pub total_assets: i64,
+
     pub accounts_payable: i64,
     pub current_long_term_debt: i64,
     pub other_current_liabilities: i64,
     pub total_current_liabilities: i64,
+
     pub long_term_debt: i64,
     pub other_liabilities: i64,
+    pub deferred_liability_charges: i64,
     pub minority_interest: i64,
     pub total_liabilities: i64,
+
+    pub redeemable_preferred_stock: i64,
+    pub preferred_stock: i64,
     pub common_stock: i64,
     pub retained_earnings: i64,
     pub treasury_stock: i64,
     pub capital_surplus: i64,
-    pub shareholder_equity: i64,
+    pub other_shareholder_equity: i64,
+    pub total_shareholder_equity: i64,
     pub net_tangible_assets: i64
+}
+
+pub struct Statistics {
+    pub shares_outstanding: u64,
+    pub market_cap: i64,
+    pub share_float: u64,
+    pub pe_ratio: f64,
+    pub beta: f64,
+
+    pub t52w_high: Money,
+    pub t52w_low: Money,
+    pub t52w_change: f64,
+
+    pub t10d_average_volume: u64,
+    pub t30d_average_volume: u64,
+
+    pub t50d_moving_average: Money,
+    pub t200d_moving_average: Money,
+
+    pub t12m_eps: Money,
+
+    pub t12m_dividend_rate: f64,
+    pub dividend_yield: f64,
+
+    pub last_ex_dividend_date: Date,
+    pub next_ex_dividend_date: Date,
+    pub next_earnings_date: Date,
+
+    pub t5d_change_percentage: f64,
+    pub t30d_change_percentage: f64,
+    pub t1m_change_percentage: f64,
+    pub t3m_change_percentage: f64,
+    pub t6m_change_percentage: f64,
+    pub t1y_change_percentage: f64,
+    pub t2y_change_percentage: f64,
+    pub t5y_change_percentage: f64,
+
+    pub ytd_change_percentage: f64,
+    pub max_change_percentage: f64
 }
 
 // ------------------------------------------------------------------------------------------------
@@ -96,5 +145,10 @@ pub trait FetchCompanyFinancials {
         -> RequestResult<Vec<Snapshot<IncomeStatement>>>;
 
     fn reported_balance_sheet(&self, for_symbol: Symbol, last: u8, period: ReportingPeriod)
-                       -> RequestResult<Vec<Snapshot<BalanceSheet>>>;
+        -> RequestResult<Vec<Snapshot<BalanceSheet>>>;
+}
+
+pub trait FetchCompanyStatistics {
+
+    fn core_statistics(&self, for_symbol: Symbol) -> RequestResult<Statistics>;
 }
