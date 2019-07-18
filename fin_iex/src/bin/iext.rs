499b140605adf3d1b0b9caf73cb7dc86f4cc8fab
Simon Johnston <johnstonskj@gmail.com>
2019-07-18 10:05:17 -0700

    added 'dyn', rustfmt

--- fin_iex/src/bin/iext.rs
+++ fin_iex/src/bin/iext.rs
@@ -9,15 +9,15 @@ use num_format::{SystemLocale, ToFormattedString};
 
 use prettytable::Table;
 
-use fin_model::prelude::*;
 use fin_model::classification::Code;
 use fin_model::market::Market;
+use fin_model::prelude::*;
 use fin_model::quote::{FetchPriceQuote, FetchPriceRangeSeries, SeriesInterval};
 
-use fin_data::markets::ISORegistry;
 use fin_data::classifiers::naics;
 use fin_data::classifiers::uk_sic;
 use fin_data::classifiers::us_sic;
+use fin_data::markets::ISORegistry;
 
 use fin_iex::IEXProvider;
 
@@ -26,23 +26,21 @@ enum Command {
     Quote(String, bool),
     Historical(String, String),
     Lookup(String, String),
-    None
+    None,
 }
 
 fn main() {
-    flexi_logger::Logger::with_env()
-        .start()
-        .unwrap();
+    flexi_logger::Logger::with_env().start().unwrap();
     info!("iext::main started");
 
     let cmd = handle_args();
 
     match cmd {
-        Command::Price(_) | Command::Quote(_,_) | Command::Historical(_,_) =>
-            provider_commands(cmd),
-        Command::Lookup(_,_) =>
-            registry_commands(cmd),
-        Command::None => ()
+        Command::Price(_) | Command::Quote(_, _) | Command::Historical(_, _) => {
+            provider_commands(cmd)
+        }
+        Command::Lookup(_, _) => registry_commands(cmd),
+        Command::None => (),
     }
 }
 
@@ -54,7 +52,7 @@ fn provider_commands(cmd: Command) {
         Err(RequestError::ConfigurationError(err)) => {
             println!("Error configuring provider: {}", err);
             return ();
-        },
+        }
         Err(err) => {
             println!("Unknown error from provider: {:?}", err);
             return ();
@@ -64,64 +62,64 @@ fn provider_commands(cmd: Command) {
     let locale = SystemLocale::default().unwrap();
 
     match cmd {
-        Command::Price(symbol) => {
-            match provider.latest_price_only(symbol) {
-                Err(e) => {
-                    println!("Call failed: {:?}", e);
-                },
-                Ok(p) => {
-                    println!("Latest price: {}", p)
-                }
+        Command::Price(symbol) => match provider.latest_price_only(symbol) {
+            Err(e) => {
+                println!("Call failed: {:?}", e);
+            }
+            Ok(p) => println!("Latest price: {}", p),
+        },
+        Command::Quote(symbol, true) => match provider.delayed(symbol) {
+            Err(e) => {
+                println!("Call failed: {:?}", e);
+            }
+            Ok(q) => {
+                println!(
+                    "Delayed price: {} (by {} minutes)",
+                    q.data.latest.price, q.data.delayed_by
+                );
+                let table = table!(
+                    ["Low", "High", "Volume"],
+                    [
+                        q.data.low,
+                        q.data.high,
+                        match q.data.volume {
+                            None => "N/A".to_string(),
+                            Some(v) => v.to_formatted_string(&locale),
+                        }
+                    ]
+                );
+                table.printstd();
             }
         },
-        Command::Quote(symbol, true) => {
-            match provider.delayed(symbol) {
-                Err(e) => {
-                    println!("Call failed: {:?}", e);
-                },
-                Ok(q) => {
-                    println!("Delayed price: {} (by {} minutes)",
-                             q.data.latest.price,
-                             q.data.delayed_by);
-                    let table = table!(
-                        ["Low", "High", "Volume"],
-                        [q.data.low,
-                         q.data.high,
-                         match q.data.volume {
-                            None => "N/A".to_string(),
-                            Some(v) => v.to_formatted_string(&locale)
-                         }]);
-                    table.printstd();
-                }
+        Command::Quote(symbol, false) => match provider.real_time(symbol) {
+            Err(e) => {
+                println!("Call failed: {:?}", e);
             }
-        },
-        Command::Quote(symbol, false) => {
-            match provider.real_time(symbol) {
-                Err(e) => {
-                    println!("Call failed: {:?}", e);
-                },
-                Ok(q) => {
-                    println!("Real-Time price: {} ({} {}%), updated {} {:?}",
-                             q.data.latest.price,
-                             q.data.latest.change.unwrap(),
-                             q.data.latest.percentage.unwrap(),
-                             q.date,
-                             q.data.latest_source);
-                    let table = table!(
-                        ["Open", "Low", "High", "Close", "Volume"],
-                        [q.data.range.open,
-                         q.data.range.low,
-                         q.data.range.high,
-                         q.data.range.close,
-                         match q.data.range.volume {
+            Ok(q) => {
+                println!(
+                    "Real-Time price: {} ({} {}%), updated {} {:?}",
+                    q.data.latest.price,
+                    q.data.latest.change.unwrap(),
+                    q.data.latest.percentage.unwrap(),
+                    q.date,
+                    q.data.latest_source
+                );
+                let table = table!(
+                    ["Open", "Low", "High", "Close", "Volume"],
+                    [
+                        q.data.range.open,
+                        q.data.range.low,
+                        q.data.range.high,
+                        q.data.range.close,
+                        match q.data.range.volume {
                             None => "N/A".to_string(),
-                            Some(v) => v.to_formatted_string(&locale)
-                         }]);
-                    table.printstd();
-                    if let Some(extended) = q.data.extended {
-                        println!(" Extended price: {}",
-                                 extended.price);
-                    }
+                            Some(v) => v.to_formatted_string(&locale),
+                        }
+                    ]
+                );
+                table.printstd();
+                if let Some(extended) = q.data.extended {
+                    println!(" Extended price: {}", extended.price);
                 }
             }
         },
@@ -138,13 +136,13 @@ fn provider_commands(cmd: Command) {
                 "5y" => SeriesInterval::FiveYears,
                 _ => {
                     warn!("invalid interval: {}", interval);
-                    return
+                    return;
                 }
             };
             match provider.last(symbol, interval) {
                 Err(e) => {
                     println!("Call failed: {:?}", e);
-                },
+                }
                 Ok(series) => {
                     let mut table = Table::new();
                     table.add_row(row!["Date", "Open", "Low", "High", "Close", "Volume"]);
@@ -157,65 +155,65 @@ fn provider_commands(cmd: Command) {
                             range.data.close,
                             match range.data.volume {
                                 None => "N/A".to_string(),
-                                Some(v) => v.to_formatted_string(&locale)
+                                Some(v) => v.to_formatted_string(&locale),
                             }
                         ]);
                     }
                     table.printstd();
                 }
             }
-        },
-        _ => ()
+        }
+        _ => (),
     }
 
-    println!("Data provided by {} ({}).", provider.attribution(), provider.url());
+    println!(
+        "Data provided by {} ({}).",
+        provider.attribution(),
+        provider.url()
+    );
 
     provider.finish();
 }
 
 fn registry_commands(cmd: Command) {
     match cmd {
-        Command::Lookup(scheme, code) => {
-            match scheme.as_str() {
-                "mic" => {
-                    let registry: ISORegistry = ISORegistry::new();
-                    registry_details(&registry);
-                    registry_lookup::<String, Market>(
-                        &code,
-                        registry.get(code.to_string()),
-                        &|m:&Market| println!("{}: {} ({})", code, m.description, m.country_code,));
-                },
-                "naics" => {
-                    let registry: naics::Scheme = naics::Scheme::new();
-                    let code = code.parse::<u32>().unwrap();
-                    registry_details(&registry);
-                    registry_lookup::<u32, Code<u32>>(
-                        &code,
-                        registry.get(code),
-                        &|c:&Code<u32>| println!("{}: {}", code, c.description));
-                },
-                "uksic" => {
-                    let registry: uk_sic::Scheme = uk_sic::Scheme::new();
-                    let code = code.parse::<u32>().unwrap();
-                    registry_details(&registry);
-                    registry_lookup::<u32, Code<u32>>(
-                        &code,
-                        registry.get(code),
-                        &|c:&Code<u32>| println!("{}: {}", code, c.description));
-                },
-                "ussic" => {
-                    let registry: us_sic::Scheme = us_sic::Scheme::new();
-                    let code = code.parse::<u16>().unwrap();
-                    registry_details(&registry);
-                    registry_lookup::<u16, Code<u16>>(
-                        &code,
-                        registry.get(code),
-                        &|c:&Code<u16>| println!("{}: {}", code, c.description));
-                },
-                _ => (),
+        Command::Lookup(scheme, code) => match scheme.as_str() {
+            "mic" => {
+                let registry: ISORegistry = ISORegistry::new();
+                registry_details(&registry);
+                registry_lookup::<String, Market>(
+                    &code,
+                    registry.get(code.to_string()),
+                    &|m: &Market| println!("{}: {} ({})", code, m.description, m.country_code,),
+                );
             }
+            "naics" => {
+                let registry: naics::Scheme = naics::Scheme::new();
+                let code = code.parse::<u32>().unwrap();
+                registry_details(&registry);
+                registry_lookup::<u32, Code<u32>>(&code, registry.get(code), &|c: &Code<u32>| {
+                    println!("{}: {}", code, c.description)
+                });
+            }
+            "uksic" => {
+                let registry: uk_sic::Scheme = uk_sic::Scheme::new();
+                let code = code.parse::<u32>().unwrap();
+                registry_details(&registry);
+                registry_lookup::<u32, Code<u32>>(&code, registry.get(code), &|c: &Code<u32>| {
+                    println!("{}: {}", code, c.description)
+                });
+            }
+            "ussic" => {
+                let registry: us_sic::Scheme = us_sic::Scheme::new();
+                let code = code.parse::<u16>().unwrap();
+                registry_details(&registry);
+                registry_lookup::<u16, Code<u16>>(&code, registry.get(code), &|c: &Code<u16>| {
+                    println!("{}: {}", code, c.description)
+                });
+            }
+            _ => (),
         },
-        _ => ()
+        _ => (),
     }
 }
 
@@ -229,64 +227,84 @@ fn handle_args() -> Command {
     let matches = App::new("iext")
         .about("IEX Tool")
         .version("v1.0-alpha")
-        .subcommand(SubCommand::with_name("price")
-            .about("Fetch latest price")
-            .arg(Arg::with_name("symbol")
-                .help("The security symbol")
-                .takes_value(true)
-                .required(true)
-                .index(1)))
-        .subcommand(SubCommand::with_name("quote")
-            .about("Fetch price quote")
-            .arg(Arg::with_name("delayed")
-                .short("d")
-                .long("delayed")
-                .help("Fetch delayed quotes only"))
-            .arg(Arg::with_name("symbol")
-                .help("The security symbol")
-                .takes_value(true)
-                .required(true)
-                .index(1)))
-        .subcommand(SubCommand::with_name("history")
-            .about("Fetch price history")
-            .arg(Arg::with_name("interval")
-                .short("i")
-                .long("interval")
-                .takes_value(true)
-                .help("The interval over which to fetch prices"))
-            .arg(Arg::with_name("symbol")
-                .help("The security symbol")
-                .required(true)
-                .index(1)))
-        .subcommand(SubCommand::with_name("lookup")
-            .about("Lookup a code from within a classification scheme")
-            .arg(Arg::with_name("scheme")
-                .short("s")
-                .long("scheme")
-                .takes_value(true)
-                .help("Classifier scheme"))
-            .arg(Arg::with_name("code")
-                .help("The classifier code")
-                .takes_value(true)
-                .required(true)))
+        .subcommand(
+            SubCommand::with_name("price")
+                .about("Fetch latest price")
+                .arg(
+                    Arg::with_name("symbol")
+                        .help("The security symbol")
+                        .takes_value(true)
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("quote")
+                .about("Fetch price quote")
+                .arg(
+                    Arg::with_name("delayed")
+                        .short("d")
+                        .long("delayed")
+                        .help("Fetch delayed quotes only"),
+                )
+                .arg(
+                    Arg::with_name("symbol")
+                        .help("The security symbol")
+                        .takes_value(true)
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("history")
+                .about("Fetch price history")
+                .arg(
+                    Arg::with_name("interval")
+                        .short("i")
+                        .long("interval")
+                        .takes_value(true)
+                        .help("The interval over which to fetch prices"),
+                )
+                .arg(
+                    Arg::with_name("symbol")
+                        .help("The security symbol")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("lookup")
+                .about("Lookup a code from within a classification scheme")
+                .arg(
+                    Arg::with_name("scheme")
+                        .short("s")
+                        .long("scheme")
+                        .takes_value(true)
+                        .help("Classifier scheme"),
+                )
+                .arg(
+                    Arg::with_name("code")
+                        .help("The classifier code")
+                        .takes_value(true)
+                        .required(true),
+                ),
+        )
         .get_matches();
 
     match matches.subcommand() {
-        ("price", Some(matches))      =>
-            Command::Price(
-                matches.value_of("symbol").unwrap().to_string()),
-        ("quote", Some(matches))      =>
-            Command::Quote(
-                matches.value_of("symbol").unwrap().to_string(),
-                matches.is_present("delayed")),
-        ("history", Some(matches))      =>
-            Command::Historical(
-                matches.value_of("symbol").unwrap().to_string(),
-                matches.value_of("interval").unwrap().to_string()),
-        ("lookup", Some(matches))     =>
-            Command::Lookup(
-                matches.value_of("scheme").unwrap().to_string(),
-                matches.value_of("code").unwrap().to_string()),
+        ("price", Some(matches)) => Command::Price(matches.value_of("symbol").unwrap().to_string()),
+        ("quote", Some(matches)) => Command::Quote(
+            matches.value_of("symbol").unwrap().to_string(),
+            matches.is_present("delayed"),
+        ),
+        ("history", Some(matches)) => Command::Historical(
+            matches.value_of("symbol").unwrap().to_string(),
+            matches.value_of("interval").unwrap().to_string(),
+        ),
+        ("lookup", Some(matches)) => Command::Lookup(
+            matches.value_of("scheme").unwrap().to_string(),
+            matches.value_of("code").unwrap().to_string(),
+        ),
         _ => {
             println!("Pick a [valid] command");
             Command::None
@@ -295,19 +313,21 @@ fn handle_args() -> Command {
 }
 // ------------------------------------------------------------------------------------------------
 
-fn registry_details<C: std::fmt::Display, T>(registry: &Registry<C, T>) {
-    println!("Registry Scheme: {} ({})", registry.name(), registry.acronym());
+fn registry_details<C: std::fmt::Display, T>(registry: &dyn Registry<C, T>) {
+    println!(
+        "Registry Scheme: {} ({})",
+        registry.name(),
+        registry.acronym()
+    );
     println!(" Governing Body: {}", registry.governing_body());
     if let Some(date) = registry.last_updated() {
         println!("   Last updated: {}", date);
     }
 }
 
-fn registry_lookup<C: std::fmt::Display, T>(code: &C, value: Option<&T>, printer: &Fn(&T)) {
+fn registry_lookup<C: std::fmt::Display, T>(code: &C, value: Option<&T>, printer: &dyn Fn(&T)) {
     match value {
-        None =>
-            println!("No value found for code {}", code),
-        Some(value) =>
-            printer(value)
+        None => println!("No value found for code {}", code),
+        Some(value) => printer(value),
     }
 }
