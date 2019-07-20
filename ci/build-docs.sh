b47ddb4882b01d44342c01c670c501b0517bc03f
Simon Johnston <johnstonskj@gmail.com>
2019-07-19 22:13:57 -0700

    externalized build scripts :(

---
+++ ci/build-docs.sh
@@ -0,0 +1,4 @@
+for CRATE in ${CRATES//,/ }
+do
+    cargo doc   --verbose --package $CRATE --no-deps
+done
---
+++ ci/deploy.sh
@@ -0,0 +1,4 @@
+for CRATE in ${CRATES//,/ }
+do
+    cargo publish --verbose --token $CARGO_TOKEN --manifest-path $CRATE/Cargo.toml
+done
