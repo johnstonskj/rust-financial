380549583cfef7e8bae5332d4b82aa3ebb8cfcd8
Simon Johnston <johnstonskj@gmail.com>
2019-07-20 22:09:45 -0700

    removed echo from deply script

--- ci/cargo-publish.sh
+++ ci/cargo-publish.sh
@@ -5,14 +5,12 @@ source ci/cargo-config.sh
 if [[ "$CARGO_TOKEN" = "" ]] ; then
     echo "Error: no CARGO_TOKEN environment variable"
     exit 2
-else
-    echo $CARGO_TOKEN
 fi
 
 if [[ $CARGO_WORKSPACE = 1 ]] ; then
     for CRATE in ${CRATES//,/ }
     do
-        echo cargo publish $CARGO_FLAGS --token $CARGO_TOKEN --manifest-path $CRATE/Cargo.toml
+        cargo publish $CARGO_FLAGS --token $CARGO_TOKEN --manifest-path $CRATE/Cargo.toml
     done
 else
     cargo publish $CARGO_FLAGS --token $CARGO_TOKEN
