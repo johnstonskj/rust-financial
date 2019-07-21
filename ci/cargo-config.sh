1ad9426fcd757e42c5ff0f25df3a5336dde450ca
Simon Johnston <johnstonskj@gmail.com>
2019-07-21 12:26:26 -0700

    documenting build scripts and bumping version number

--- ci/cargo-config.sh
+++ ci/cargo-config.sh
@@ -2,8 +2,22 @@
 
 if $(grep -q "^\[workspace\]$" Cargo.toml) ; then
     export CARGO_WORKSPACE=1
-    export CRATES=$(cat Cargo.toml | egrep -o '"[^"]+"' | tr '"' ' ' |tr '\n' ' ' | tr -s '[:space:]' | sed -e 's/^[[:space:]]*//' |sed -e 's/[[:space:]]*$//' | tr ' ' ',')
-    echo "workspace contains $CRATES"
+    #
+    # This will extract the set of members from the workspace's Cargo.toml
+    # file, it will construct a comma separated list in the $CRATES
+    # environment variable, these SHOULD be listed in order of dependency
+    # in your workspace so that publish steps can work effectively.
+    #
+    export CRATES=$(cat Cargo.toml \
+        | egrep -o '"[^"]+"' \
+        | tr '"'  ' ' \
+        | tr '\n' ' ' \
+        | tr -s '[:space:]' \
+        | sed -e 's/^[[:space:]]*//' \
+        | sed -e 's/[[:space:]]*$//' \
+        | tr ' ' ',' \
+    )
+    echo "Cargo workspace contains ( $CRATES ) crates"
 else
     export CARGO_WORKSPACE=0
 fi
